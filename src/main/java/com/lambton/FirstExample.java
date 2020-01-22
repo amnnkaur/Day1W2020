9c20d0afb5c97b1142b48f2c6ffc7e491bf38bb3
Amanpreet Kaur <amankaur2628@gmail.com>
2020-01-22 18:46:53 -0500

    Day 2 commit(1)

--- src/main/java/com/lambton/FirstExample.java
+++ src/main/java/com/lambton/FirstExample.java
@@ -10,6 +10,47 @@ public class FirstExample
         System.out.println("Amanpreet Kaur");
         }
          System.out.println("Hello!!");
+         //Day2
+        int a=100;
+        int b;
+        b=200;
+        int c=a+b;
+
+        System.out.println("Sum =" +a +b);
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+        short x= 32454;
+        System.out.println("Short Max" + Short.MAX_VALUE);
+        System.out.println("Short MIN=" + Short.MIN_VALUE);
+
+        long y= 556777648;
+        System.out.println(y);
+
+        float z= 300.50F;
+        double q= 1000.0;
+        System.out.println("z=" + z);
+        System.out.println("q=" + q);
+
+        int e= 10/3;
+        float w= 10/3;
+
+        float r= 10.0F/3.0F;
+        float t= 10.0F/3;
+
+        int u= (int) (10/3.0F);
+        double o= 1_00_00.50/10;
+
+        System.out.println("w=" + w);
+        System.out.println("e=" + e);
+        System.out.println("r=" + r);
+        System.out.println("t=" + t);
+        System.out.println("u=" + u);
+        System.out.println("o=" + o);
+
+
     }
 }
 
